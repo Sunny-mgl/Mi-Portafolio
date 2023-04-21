7e25d50a1a3b5aaf9f841c0461f0ba8effbc1d52
Sunny-mgl <sunnymartinez@gmail.com>
2023-04-21 17:02:08 -0500

    about-update

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -19,20 +19,27 @@ const Aboutme = () => {
         <div className='aboutme-container' id='about' >
             <div className='hey2'>
                 <div className='aboutme2-container'>
-              
+
 
                     <div data-aos="flip-left" className='info'>
                         <div className='aboutp-container'>
-                            <p className='aboutMe'>
+                            <div className='aboutMe'>
 
-                                <span  className='aboutmeh1'>
-                                   <button onClick={skill} className='btn-skill'></button>
-                                   Desarrolladora Full Stack,
+                                <span className='aboutmeh1'>
+                                    <button onClick={skill} className='btn-skill'></button>
+                                    Desarrolladora Full Stack,
                                 </span>
-
-                                 {' '} {' '}
-                                La programación es uno de los aspectos de mi vida que más me apasiona, me gusta porque es algo que me permite aprender algo nuevo cada día, otra cosa muy importante es que la programación me ha ayudado a aprender lo importante que es el trabajo en equipo y todo el aprendizaje que puede traer consigo, me considero una persona que puede llegar a ser muy perfeccionista y siempre trato de aprender algo nuevo y así poder crecer tanto en lo profesional como en lo personal
-                            </p>
+                            
+                                La programación es uno de los aspectos de mi vida que más me gusta ya que me permite aprender algo nuevo cada día, siempre trato de aprender algo nuevo y así poder crecer tanto en lo profesional como en lo personal
+                                <p className='container-infoabout'> <span className='info-about'> <i className="fa-solid fa-phone fa-lg"></i>  +593 992444232 </span></p>
+                                <p className='container-infoabout'><span className='info-about'> <i className="fa-solid fa-location-dot fa-lg"></i>  Ecuador </span></p>
+                                <p className='container-infoabout'><span className='info-about'> <i className="fa-solid fa-envelope fa-lg"></i> martinezgilersunny@gmail.com </span></p>
+                                <p className='container-infoabout'> <span className='info-about'> <i className="fa-solid fa-language fa-lg"></i> English B1, Español</span></p>
+                              
+                                {' '}
+                                
+                            </div>
+  <br />
                         </div>
                         <div data-aos="flip-left" className='foto-about'>
                             <img src={sunny} className='foto' alt="" />
